f958d7f64905386342cf2458b5af3c07532a8383
izalouyza <izadora.figueiredo@alunos.ufersa.edu.br>
2026-06-15 17:18:13 -0300

    fix: resolvendo bugs e erros

--- src/backend/src/routes/agendamentoRoutes.js
+++ src/backend/src/routes/agendamentoRoutes.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const agendamentoController = require('../controllers/agendamentoController');
-const { verificarToken } = require('../middlewares/authMiddleware');
 
-// POST /api/agendamentos (Aluno marca o atendimento)
-router.post('/', verificarToken, agendamentoController.agendarAtendimento);
-
-// PUT /api/agendamentos/:id_agendamento/cancelar (Aluno cancela a sua vaga)
-router.put('/:id_agendamento/cancelar', verificarToken, agendamentoController.cancelarAgendamento);
+router.post('/', agendamentoController.agendar);
 
 module.exports = router;
